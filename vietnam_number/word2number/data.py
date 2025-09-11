66369ea52991ce417c4067a81bff57997c5a93b6
thuong.nguyen <thuong.nguyen@sungrove.co.jp>
2025-09-11 11:23:18 +0900

    add more data

--- vietnam_number/word2number/data.py
+++ vietnam_number/word2number/data.py
@@ -10,6 +10,7 @@ units = {
     'rưởi': 5,
     'rưỡi': 5,
     'lăm': 5,
+    'nhăm': 5,
     'sáu': 6,
     'bảy': 7,
     'tám': 8,
@@ -18,11 +19,11 @@ units = {
 
 billion_words = frozenset(("tỷ", "tỏi", "tỉ"))
 million_words = frozenset(("triệu", "củ", "chai"))
-thousand_words = frozenset(("nghìn", "nghàn", "ngàn"))
+thousand_words = frozenset(("nghìn", "nghàn", "ngàn", "cành"))
 
 BILLION_MILLION_THOUSAND_WORDS = billion_words.union(million_words, thousand_words)
 
-hundreds_words = frozenset(("trăm", "lít"))
+hundreds_words = frozenset(("trăm", "lít", "lốp", "xị"))
 tens_words = frozenset(("mươi", "chục"))
 
 HUNDREDS_TENS_WORDS = hundreds_words.union(tens_words)
