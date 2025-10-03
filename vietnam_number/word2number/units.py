f538a5b0bb28ca9c692281adef7fcb9780f24ccf
Vu Hoang Minh <your_email@example.com>
2025-10-03 20:14:33 +0700

    change argument from `Sequence[str]` to `str` to simpify function `process_units`

--- vietnam_number/word2number/units.py
+++ vietnam_number/word2number/units.py
@@ -1,26 +1,15 @@
-from collections.abc import Sequence
-
 from vietnam_number.word2number.data import UNITS
 
 
-def process_units(words: Sequence[str]) -> str:
+def process_units(words: str) -> str:
     """Xữ lý chữ số hàng đơn vị.
 
     Args:
-        words (Sequence[str]): Danh sách chữ số đầu vào.
+        words (str): Danh sách chữ số đầu vào.
 
     Returns:
         Chuổi số hàng đơn vị.
 
-    Raises:
-        ValueError: Danh sách chữ số đầu vào lớn hơn 1.
-
     """
-    # nếu list truyền vào là rỗng thì bằng 0
-    # Optimal order: highest frequency first
-    if len(words) == 1:
-        return str(UNITS[words[0]])
-    elif not words:
-        return "0"
-    else:
-        raise ValueError('chữ số vượt quá hàng đơn vị')
+    # nếu str truyền vào là rỗng thì bằng "0"
+    return str(UNITS.get(words, "0"))
